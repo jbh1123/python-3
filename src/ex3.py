d403558484f9f08dbed6e09dc56b741ec56fe02a
Herman <julian.herman@fiserv.com>
2023-11-24 14:19:21 -0500

    implemented ex4

--- src/ex3.py
+++ src/ex3.py
@@ -1,11 +1,27 @@
+import csv
 
-def create_files():
-    pass # TODO
+def count_words(input_file):
+    set_of_words = set()
 
+    with open(input_file) as csv_file:
+        rows = csv.reader(csv_file, delimiter=' ')
+        for row in rows:
+            set_of_words.update(row)
 
+    set_of_words.remove('')
+    small_words = set(filter(lambda w: len(w) < 3, set_of_words))
+    large_words = set(set_of_words.difference(small_words))
+
+    with open("files/large-words.txt", mode='w') as csv_file:
+        large_writer = csv.writer(csv_file, delimiter='\n')
+        large_writer.writerows([large_words])
+
+    with open("files/small-words.txt", mode='w') as csv_file:
+        small_writer = csv.writer(csv_file, delimiter='\n')
+        small_writer.writerows([small_words])
+    
+    return len(set_of_words)
 
 def ex3():
-    total_words = create_files("words.txt")
+    total_words = count_words("files/words.txt")
     print(f"Total words: {total_words}.")
-
-ex3()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from ex1 import ex1
 from ex2 import ex2
+from ex3 import ex3
 
 if __name__ == '__main__':
     ex1()
     ex2()
+    ex3()
