a6d81b0fb86ae35ba456f31c09323d48c02ba8dd
Herman <julian.herman@fiserv.com>
2023-11-24 15:07:43 -0500

    implemented ex5

--- src/ex5.py
+++ src/ex5.py
@@ -1,11 +1,22 @@
+import csv
 from pprint import pprint
 
 def build_car_list():
-    pass # TODO
+    car_list = []
+    id_model_mapping = dict()
+    with open("files/car-ids.txt") as csv_file:
+        rows = csv.reader(csv_file, delimiter=',')
+        for row in rows:
+            id_model_mapping.update({row[0]: row[1].strip()})
 
+    with open("files/input.txt") as csv_file:
+        rows = list(csv.reader(csv_file, delimiter=','))
+        for row in rows[1:]:
+            if float(row[1]) % 1.0 == 0.0:
+                car_list.append({'id': int(row[0]), 'miles': int(row[1]), 'model': id_model_mapping.get(row[0])})
+
+    return car_list
 
 def ex5():
     car_list = build_car_list()
     pprint(car_list)
-
-ex5()
--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from ex1 import ex1
 from ex2 import ex2
 from ex3 import ex3
 from ex4 import ex4
+from ex5 import ex5
 
 if __name__ == '__main__':
     ex1()
     ex2()
     ex3()
     ex4()
+    ex5()
