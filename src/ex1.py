b90281fa77665e330568209163498e105db11b04
Herman <julian.herman@fiserv.com>
2023-11-24 13:10:44 -0500

    implemented ex1

--- src/ex1.py
+++ src/ex1.py
@@ -1,15 +1,16 @@
 from ValidationException import ValidationException
+import csv
 
-
-def validate_file():
-    pass # TODO
-
+def validate_file(input_file):
+    with open(input_file) as csv_file:
+        rows = list(csv.reader(csv_file, delimiter=','))
+        for row in rows[1:]:
+            if float(row[1]) % 1.0 != 0.0:
+                raise ValidationException(f"Invalid mileage: {row[1]}")
 
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("files/input.txt")
     except ValidationException as ve:
         print(ve)
-
-ex1()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from ex1 import ex1
+
+if __name__ == '__main__':
+    ex1()
